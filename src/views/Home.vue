f1d6ef3042d0dde2c44b0c32384a87e41b1926f4
Gabriant <antoniogabriel@outlook.com>
2020-03-26 20:22:05 -0500

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container class="mt-5">
+      Insira sua frase:
+      <b-form-input v-model="text"></b-form-input>
+
+      Trocar por:
+      <b-form-input v-model="replace"></b-form-input>
+      <b-form-checkbox
+        id="checkbox-1"
+        v-model="toggle"
+      >Apenas Minusculas</b-form-checkbox>
+
+      <b-card class="mt-3">
+        Resultado:
+        <h2>{{ getAsterisk }}</h2>
+
+      </b-card>
+    </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      text: '',
+      toggle: false,
+      replace: '*'
+    }
+  },
+
+  computed: {
+    getAsterisk () {
+      let result
+      if (this.toggle) {
+        result = this.text.replace(/([a-z])/g, this.replace)
+      } else {
+        result = this.text.replace(/([A-Z]|[a-z])/g, this.replace)
+      }
+      return result
+    }
   }
 }
 </script>
